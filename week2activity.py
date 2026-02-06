bb7385e555b948d9cd39858a40ee0b26f010d568
avricugsod <ugsodavricrey@gmail.com>
2026-02-06 14:03:57 +0800

    Add files via upload

---
+++ week2activity.py
@@ -0,0 +1,16 @@
+fullname = "AVRIC REY B. UGSOD"
+age = 19
+height = 1.77
+currently_working = False
+
+print("Fullname:", fullname)
+print(type(fullname))
+
+print("\nAge:", age)
+print(type(age))
+
+print("\nHeight in meters:", height)
+print(type(height))
+
+print("\nCurrently working?", currently_working)
+print(type(currently_working))
